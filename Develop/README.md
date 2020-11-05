d58b4d0b91dcd8d59245ca31323601d5d28e2fe9
CBanks <banksc7669@yahoo.com>
2020-11-05 18:54:37 -0500

    initial push

---
+++ Develop/README.md
@@ -0,0 +1,4 @@
+# something
+
+  ## Description
+  idk
---
+++ Develop/index.js
@@ -0,0 +1,41 @@
+const inquirer = require("inquirer");
+var generator = require("./utils/generateMarkdown");
+var fs = require("fs");
+
+// array of questions for user
+const questions = [{
+    name:"title",
+    type:"input",
+    message:"What is the title of your project?"
+},
+{
+    name:"description",
+    type:"input",
+    message:"Type a description for your project"
+}
+
+];
+
+let inquirerData;
+
+// function to write README file
+function writeToFile(fileName = './README.md', data = inquirerData) {
+    fs.writeFileSync(fileName, data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+    });
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(function(answers){
+        console.log(answers)
+        let inquirerData = generator(answers)
+    writeToFile('./README.md',inquirerData)
+    }).catch(function(err){
+        if (err) throw err
+
+    })}
+
+// function call to initialize program
+init();
