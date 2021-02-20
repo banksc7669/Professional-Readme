fbbb8f8ebae34e0ea5487e3bfbc0450988a5ea9f
CBanks <banksc7669@yahoo.com>
2021-02-20 10:49:03 -0500

    debug generateReadme.js

--- Develop/utils/generateReadme.js
+++ Develop/utils/generateReadme.js
@@ -3,9 +3,10 @@ function generateReadme(answers) {
   return `
 <h1 align="center">${answers.projectTitle} </h1>
 
-![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
 ## Description
 ${answers.description}
+
 ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
@@ -14,26 +15,32 @@ ${answers.description}
 - [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
+
 ## Installation
 ${answers.installation}
+
 ## Usage
 ${answers.usage}
+
 ## License
-![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+(https://img.shields.io/badge/license-${answers.license}-brightgreen)
 <br />
 This application is covered by the ${answers.license} license. 
+
 ## Contributing
 ${answers.contributing}
+
 ## Tests
 ✏️ ${answers.tests}
+
 ## Questions
 ${answers.questions}<br />
+
 <br />
-:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+:octocat: Find me on GitHub: [${answers.username}]( https://github.com/banksc7669/${answers.username})<br />
 <br />
-✉️ Email me with any questions: ${answers.email}<br /><br />
-_This README was generated with ❤️ by [README-generator](https://github.com/jpd61/README-generator) 🔥🔥🔥_
-  `;
+Email me with any questions: ${answers.email}<br /><br />
+`;
 }
 
-module.exports = generateReadme;
+module.exports = generateMarkdown;
